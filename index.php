b8d3e394023cc43a0a178c43f41535766000034c
Johay90 <johay.jjj@googlemail.com>
2015-07-06 18:20:41 +0100

    Upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <head>
+    <title>My Battle Rating Progress</title>
+    <link rel="Stylesheet" href="style.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:700italic,300,400' rel='stylesheet' type='text/css'>
+  </head>
+
+  <div id="main">
+
+    <div id="h-content">
+
+      <h1 id="header">My Wartune Progress</h1>
+      <p id="header">This website is personal website to keep track of my progress on a Browser Turn based MMO game called Wartune. It has something called "Battle Rating" which in essence is your "toons score" ie. Your combined gear score, your pets score etc. I want to track this on a day by day basis to see how I'm upgrading.</p>
+
+    </div>
+
+      <div id="main-content">
+        <table>
+          <th>Todays Date</th>
+          <th>Battle Rating</th>
+          <th>Comments</th>
+
+          <tr>
+          <td>28th Sempt </td>
+          <td>153k </td>
+          <td>My starting poiint for checking where i'm up to. Waiting for big events such as Crystals/Mount/Tech. Todays event were just diamond which gave me +3k. Will get another piece of 50 leg gear tonight. Should push me above 155k for tomorrow. Aiming for 200k on the 14th.</td>
+        </table>
+    </div>
+
+    <div id="inputdata">
+      
+    </div>
+
+  </div>
