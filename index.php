93b18b927c5b80d2535506081727dcb0aa6d89d8
Johay90 <johay.jjj@googlemail.com>
2015-07-08 16:16:00 +0100

    table plus styling
    Server side code should finish it off tomorrow (then just advanced
    features like comparing, graphs etc)

--- index.php
+++ index.php
@@ -11,26 +11,41 @@
 
     <div id="h-content">
 
-      <h1 id="header">My Wartune Progress</h1>
-      <p id="header">This website is personal website to keep track of my progress on a Browser Turn based MMO game called Wartune. It has something called "Battle Rating" which in essence is your "toons score" ie. Your combined gear score, your pets score etc. I want to track this on a day by day basis to see how I'm upgrading.</p>
+      <h1>My Wartune Progress</h1>
+      <p>This website is personal website to keep track of my progress on a Browser Turn based MMO game called Wartune. It has something called "Battle Rating" which in essence is your "toons score" ie. Your combined gear score, your pets score etc. I want to track this on a day by day basis to see how I'm upgrading.</p>
 
     </div>
 
-      <div id="main-content">
+    <div id="contentarea">
         <table>
           <th>Todays Date</th>
           <th>Battle Rating</th>
           <th>Comments</th>
 
           <tr>
-          <td>28th Sempt </td>
+          <td>06/07/15 </td>
           <td>153k </td>
-          <td>My starting poiint for checking where i'm up to. Waiting for big events such as Crystals/Mount/Tech. Todays event were just diamond which gave me +3k. Will get another piece of 50 leg gear tonight. Should push me above 155k for tomorrow. Aiming for 200k on the 14th.</td>
+          <td>My starting poiint for checking where i'm up to. Waiting for bigs events such as Crystals/Mount/Tech. Todays event were just diamond which gave me +3k. Will get another piece of 50 leg gear tonight. Should push me above 155k for tomorrow. Aiming for 200k on the 14th.</td>
+
+          <tr>
+          <td>07/07/15  </td>
+          <td>156k </td>
+          <td>Got my new piece plus kid gems (few lvl 5s). Don't expect much of a BR boost until next event. No exciting events currently.</td>
+
+          <tr>
+          <td>08/07/15  </td>
+          <td>157k </td>
+          <td>As noted y/d. I woudn't get much today. However whip event tomorrow so should be going big. Ideally I want 175/180k from it. Hopefully Soul event soon which should put me around 200k. If I don't hit at least 170 tomorrow I'll be disappointed.</td>
         </table>
-    </div>
+      </div>
 
     <div id="inputdata">
-      
+      <form>
+        <input placeholder="BR..." type="text" name="newbr">
+        <textarea placeholder="Comments..." cols="65" rows="15"></textarea><br />
+        <input type="submit" value="Submit">
+      </form>
+
     </div>
 
   </div>
